df2d85e527683e5f6dee405d73f112baba36acc4
Ganesh S Kudva <ganeshskudva@gmail.com>
2021-04-15 06:32:03 -0700

    Create L509.go

---
+++ Medium/L509.go
@@ -0,0 +1,19 @@
+package Medium
+
+func fib(n int) int {
+	mp := make(map[int]int)
+	
+	mp[0], mp[1], mp[2] = 0, 1, 1
+	
+	return fibo(mp, n)
+}
+
+func fibo(mp map[int]int, n int) int {
+	if _, ok := mp[n]; ok {
+		return mp[n]
+	}
+	
+	mp[n] = fibo(mp, n-1) + fibo(mp, n-2)
+	
+	return mp[n]
+}
