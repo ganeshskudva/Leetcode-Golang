096539cd68723d81372b895ea9c59476c6e2d4fd
Ganesh S Kudva <ganeshskudva@gmail.com>
2021-07-19 14:50:16 -0700

    Create L235.go

---
+++ Easy/L235.go
@@ -0,0 +1,20 @@
+package Easy
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root.Val > p.Val && root.Val > q.Val {
+		return lowestCommonAncestor(root.Left, p, q)
+	} else if root.Val < p.Val && root.Val < q.Val {
+		return lowestCommonAncestor(root.Right, p, q)
+	} else {
+		return root
+	}
+}
