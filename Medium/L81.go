cc3cf83ccb44fc7a95df6431dbe64836cee77dd1
Ganesh S Kudva <ganeshskudva@gmail.com>
2022-03-27 19:03:54 -0700

    Create L81.go

---
+++ Medium/L81.go
@@ -0,0 +1,29 @@
+package Medium
+
+func search(nums []int, target int) bool {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return true
+		}
+
+		if nums[left] < nums[mid] {
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else if nums[left] > nums[mid] {
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			left++
+		}
+	}
+
+	return false
+}
