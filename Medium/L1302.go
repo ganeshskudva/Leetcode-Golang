b6dc813b67f4b14dbe16b3dac22d40b4a1f02801
Ganesh S Kudva <ganeshskudva@gmail.com>
2022-05-15 08:22:25 -0700

    Update L1302.go

--- Medium/L1302.go
+++ Medium/L1302.go
@@ -9,34 +9,33 @@ package Medium
  * }
  */
 func deepestLeavesSum(root *TreeNode) int {
-    	if root == nil {
+	if root == nil {
 		return 0
 	}
-	
-	sum := 0
+
 	var q []*TreeNode
 	q = append(q, root)
-	
+	sum := 0
+
 	for len(q) > 0 {
 		size := len(q)
 		sum = 0
 		for ; size > 0; size-- {
-			n := q[0]
+			node := q[0]
 			q = q[1:]
-			
-			if n.Left == nil && n.Right == nil {
-				sum += n.Val
-			}
-			
-			if n.Left != nil {
-				q = append(q, n.Left)
-			}
-			
-			if n.Right != nil {
-				q = append(q, n.Right)
+
+			if node.Left == nil && node.Right == nil {
+				sum += node.Val
+			} else {
+				if node.Left != nil {
+					q = append(q, node.Left)
+				}
+				if node.Right != nil {
+					q = append(q, node.Right)
+				}
 			}
 		}
 	}
-	
+
 	return sum
 }
