35365a3297daa69d4e2f7f896dfa6c2503d5d276
Ganesh S Kudva <ganeshskudva@gmail.com>
2022-05-23 16:05:40 -0700

    Update L474.go

--- Medium/L474.go
+++ Medium/L474.go
@@ -34,3 +34,58 @@ func max(a, b int) int {
 
 	return b
 }
+
+
+// Recursion + Memoization
+func findMaxForm(strs []string, m int, n int) int {
+	if len(strs) == 0 {
+		return 0
+	}
+
+	return solve(strs, make(map[string]int), m, n, 0)
+}
+
+func solve(strs []string, mp map[string]int, m, n, index int) int {
+	// If we are done with the our remaining zeros and ones return 0 as we cant get any furtherr strings.
+	if m == 0 && n == 0 {
+		return 0
+	}
+
+	if index >= len(strs) {
+		return 0
+	}
+
+	key := fmt.Sprintf("%d:%d:%d", m, n, index)
+	if _, ok := mp[key]; ok {
+		return mp[key]
+	}
+
+	// For the current index count the required number of zeros and ones .
+	totalCnt, curr := 0, strs[index]
+	ones, zeroes := 0, 0
+
+	for i := range curr {
+		if curr[i] == '0' {
+			zeroes++
+		} else {
+			ones++
+		}
+	}
+
+	takenStrCnt := 0
+	// if we have the  required number of zeros and ones  we take the current string and update the
+	// remaining zeros and ones and go to the next index
+	if ones <= n && zeroes <= m {
+		takenStrCnt += 1 + solve(strs, mp, m-zeroes, n-ones, index+1)
+	}
+
+	skippedStrNum := solve(strs, mp, m, n, index+1)
+	totalCnt = skippedStrNum
+	// For every position we also the option to skip the current string
+	if takenStrCnt > skippedStrNum {
+		totalCnt = takenStrCnt
+	}
+
+	mp[key] = totalCnt
+	return totalCnt
+}
