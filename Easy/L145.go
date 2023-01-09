023d218d9f4c6f61cf0a1a6cbee77ab3960dba31
Ganesh S Kudva <ganeshskudva@gmail.com>
2023-01-09 05:20:21 -0800

    Update L145.go

--- Easy/L145.go
+++ Easy/L145.go
@@ -1,22 +1,50 @@
 package Easy
 
-func postorderTraversal(root *TreeNode) []int {
-	var res []int
-	if root == nil {
-		return res
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// Iterative Solution
+func preorderTraversal(root *TreeNode) []int {
+	var (
+		res []int
+		st []*TreeNode
+	)
+	st = append(st, root)
+	
+	for len(st) > 0 {
+		node := st[len(st) - 1]
+		st = st[:len(st) - 1]
+		if node != nil {
+			res = append(res, node.Val)
+			st = append(st, node.Right)
+			st = append(st, node.Left)
+		}
 	}
-
-	postOrder(root, &res)
-
+	
 	return res
 }
 
-func postOrder(root *TreeNode, list *[]int) {
+
+// Recursive Solution
+func preorderTraversal(root *TreeNode) []int {
+	var res []int
+
+	solve(root, &res)
+	return res
+}
+
+func solve(root *TreeNode, res *[]int) {
 	if root == nil {
 		return
 	}
 
-	postOrder(root.Left, list)
-	postOrder(root.Right, list)
-	*list = append(*list, root.Val)
+	*res = append(*res, root.Val)
+	solve(root.Left, res)
+	solve(root.Right, res)
 }
