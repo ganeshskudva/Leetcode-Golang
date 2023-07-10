780e4f412275a72fda40cbc3a0bc215e538e5111
Ganesh S Kudva <ganeshskudva@gmail.com>
2023-07-10 16:13:43 -0700

    Create L111.go

---
+++ Easy/L111.go
@@ -0,0 +1,41 @@
+package Easy
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	
+	var q []*TreeNode
+	lvl := 1
+	q = append(q, root)
+	
+	for len(q) > 0 {
+		size := len(q)
+		for i := 0; i < size; i++ {
+			node := q[0]
+			q = q[1:]
+			
+			if node.Left == nil && node.Right == nil {
+				return lvl
+			}
+			
+			if node.Left != nil {
+				q = append(q, node.Left)
+			}
+			if node.Right != nil {
+				q = append(q, node.Right)
+			}
+		}
+		lvl++
+	}
+	
+	return lvl
+}
