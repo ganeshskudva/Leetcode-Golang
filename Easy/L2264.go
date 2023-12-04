ebf6b52bd527c5b6fcf998874fb310135539feb7
Ganesh S Kudva <ganeshskudva@gmail.com>
2023-12-04 12:48:09 -0800

    Create L2264.go

---
+++ Easy/L2264.go
@@ -0,0 +1,16 @@
+package Easy
+
+func largestGoodInteger(num string) string {
+	res := -1
+	for i := 0; i+2 < len(num); i++ {
+		if num[i] == num[i+1] && num[i+1] == num[i+2] {
+			res = max(res, int(num[i]-'0'))
+		}
+	}
+
+	if res == -1 {
+		return ""
+	}
+
+	return fmt.Sprintf("%d%d%d", res, res, res)
+}
