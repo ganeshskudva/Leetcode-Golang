04f648502dddac646956271f6b5577af0213c3dc
Ganesh S Kudva <ganeshskudva@gmail.com>
2024-05-17 16:53:16 -0700

    Create L1325.go

---
+++ Medium/L1325.go
@@ -0,0 +1,24 @@
+package Medium
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func removeLeafNodes(root *TreeNode, target int) *TreeNode {
+	if root.Left != nil {
+		root.Left = removeLeafNodes(root.Left, target)
+	}
+	if root.Right != nil {
+		root.Right = removeLeafNodes(root.Right, target)
+	}
+	
+	if root.Left == root.Right && root.Val == target {
+		return nil
+	}
+	
+	return root
+}
